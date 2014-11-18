d5dd24d6eb291ba520c16b18663ed2a992fb1f6f
Matthew Bunday <zenceph@gmail.com>
2014-11-18 10:47:10 -0500

    Add update routes.

--- app/controllers/wiki.rb
+++ app/controllers/wiki.rb
@@ -1,15 +1,29 @@
+# Create
 get '/wiki/new' do
   erb :'wiki/new_form'
-end
-
-get '/wiki/:id' do
-  @wiki = Wiki.find(params[:id])
-  erb :'wiki/show'
 end
 
 post '/wiki' do
   p params
   p params[:wiki]
   wiki = Wiki.create(params[:wiki])
+  redirect("/wiki/#{wiki.id}")
+end
+
+# Read
+get '/wiki/:id' do
+  @wiki = Wiki.find(params[:id])
+  erb :'wiki/show'
+end
+
+# Update
+get '/wiki/:id/edit' do
+  @wiki = Wiki.find(params[:id])
+  erb :'wiki/update_form'
+end
+
+put '/wiki/:id' do
+  wiki = Wiki.find(params[:id])
+  wiki.update(params[:wiki])
   redirect("/wiki/#{wiki.id}")
 end
