da6b59801c2b02f57a337ae96096989c042a0039
Matthew Bunday <zenceph@gmail.com>
2014-11-18 10:38:31 -0500

    Add post route.

--- app/controllers/wiki.rb
+++ app/controllers/wiki.rb
@@ -1,4 +1,15 @@
+get '/wiki/new' do
+  erb :'wiki/new_form'
+end
+
 get '/wiki/:id' do
   @wiki = Wiki.find(params[:id])
   erb :'wiki/show'
+end
+
+post '/wiki' do
+  p params
+  p params[:wiki]
+  wiki = Wiki.create(params[:wiki])
+  redirect("/wiki/#{wiki.id}")
 end
