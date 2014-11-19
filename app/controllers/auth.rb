51e2774c35a2f69941e52eb70f020a6e9b5bd5a1
Matthew Bunday <zenceph@gmail.com>
2014-11-19 09:20:11 -0500

    Working on signup controller.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,5 +7,16 @@ get '/login' do
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect("/")
+  else
+    session[:error] = user.errors.messages 
+    redirect("/signup")
+  end
 end
