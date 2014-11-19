650d84d72a2def1e68c5e696caa32e1fbf79f968
Matthew Bunday <zenceph@gmail.com>
2014-11-19 14:36:25 -0500

    Add contribution model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_many :contributions
   # Remember to create a migration!
+  has_many :contributed_wikis, through: :contributions, class_name: "Wiki", source: :wiki
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,12 @@
 class Wiki < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
+
+  has_many :contributions
+  #has_many :users, through: :contributions, class_name: "User", source: :user
+  has_many :contributers, through: :contributions, class_name: "User", source: :user
+  # has_many :contributers, through: :contributions, class_name: "Contributer", source: :contributer
+
+  # has_many :subscriptions
+  # has_many :users, through: :subscriptions
 end
