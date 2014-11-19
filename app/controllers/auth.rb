e87105c52c65397bbd210a9cfb72da3c62e322f2
Matthew Bunday <zenceph@gmail.com>
2014-11-19 09:47:12 -0500

    Add more auth controllers.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,11 +3,28 @@ get '/' do
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name]).try(:authenticate, params[:user][:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect("/")
+  else
+    set_error("Username not found or password incorrect.")
+    redirect("/login")
+  end
 end
 
 get '/signup' do
   erb :'auth/signup'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect("/")
 end
 
 post '/signup' do
