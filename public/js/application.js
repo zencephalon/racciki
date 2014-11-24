abfd07f3b957c2851a7b04363a33887013ad7f67
Matthew Bunday <zenceph@gmail.com>
2014-11-24 15:08:19 -0500

    Delete via AJAX.

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('form.delete_form').submit(function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+    $target.find("input[type=submit]").val("Deleting...");
+
+    $.ajax({
+      type: "DELETE",
+      url: $target.attr("action"),
+      data: {hello: "value"}
+    }).done(function(response) {
+      console.log(response);
+      $target.parent().remove();
+    })
+
+    console.log("Hello from delete form handler.")
+  });
 });
